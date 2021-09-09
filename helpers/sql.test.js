b707d38b4c9871cc7dc7bc7fdd2dd8f09f046946
Januar Soepangat <januar.soepangat@gmail.com>
2021-09-09 16:08:25 -0500

    test sqlForPartialUpdate()

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,7 +1,13 @@
 const { sqlForPartialUpdate } = require("./sql");
 
 describe("sqlForPartialUpdate", () => {
-    test("what am i testing?", () => {
-        
-    })
-})
+    test("works: 1 item", function () {
+        const result = sqlForPartialUpdate(
+            { firstName: "Aliya" },
+            { firstName: "firstName", fF2: "f2" });
+        expect(result).toEqual({
+          setCols: "\"firstName\"=$1",
+          values: ["Aliya"],
+        });
+      });
+});
