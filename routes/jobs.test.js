43b0e9f9924d999866057128e45e3452731852a6
Januar Soepangat <januar.soepangat@gmail.com>
2022-05-02 12:36:45 -0500

    update tests

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -60,19 +60,103 @@ describe("POST /jobs", () => {
 /************************************** GET /jobs */
 
 describe("GET /jobs", () => {
-
+  test("works", async function() {
+    const resp = await request(app).get(`/jobs`);
+    expect(resp.body).toEqual({
+      jobs: [
+        {
+          id: expect.any(Number),
+          title: "Job1",
+          salary: null,
+          equity: null,
+          companyHandle: "c1",
+        },
+        {
+          id: expect.any(Number),
+          title: "Job2",
+          salary: 100000,
+          equity: "0",
+          companyHandle: "c1",
+        },
+        {
+          id: expect.any(Number),
+          title: "Job3",
+          salary: 200000,
+          equity: "0.1",
+          companyHandle: "c1",
+        },
+        {
+          id: expect.any(Number),
+          title: "Job4",
+          salary: 300000,
+          equity: "0.2",
+          companyHandle: "c1",
+        },
+      ]
+    })
+  })
 });
 
 /************************************** GET /jobs/:id */
 
 describe("GET /jobs/:id", () => {
+  test("works", async function() {
+    const resp = await request(app).get(`/jobs/${testJobIds[0]}`);
+    expect(resp.body).toEqual({
+      job: {
+        id: testJobIds[0],
+        title: "Job1",
+        salary: null,
+        equity: null,
+        company: {
+          handle: "c1",
+          name: "C1",
+          description: "Desc1",
+          numEmployees: 1,
+          logoUrl: "http://c1.img",
+        }
+      }
+    })
+  })
 
+  test("job not found", async function() {
+    const resp = await request(app).get(`/jobs/0`);
+    expect(resp.statusCode).toEqual(404);
+  })
 });
 
 /************************************** PATCH /jobs/:id */
 
 describe("PATCH /jobs/:id", () => {
+  test("works for admin", async function() {
+    const resp = await request(app)
+        .patch(`/jobs/${testJobIds[0]}`)
+        .send({
+          title: "update",
+          salary: 100,
+          equity: "0.2",
+        })
+        .set("authorization", `Bearer ${adminToken}`);
+    expect(resp.body).toEqual({
+      job: {
+        id: expect.any(Number),
+        title: "update",
+        salary: 100,
+        equity: "0.2",
+        companyHandle: "c1",
+      }
+    });
+  });
 
+  test("not work for non-admin", async function() {
+    const resp = await request(app)
+        .patch(`/jobs/${testJobIds[0]}`)
+        .send({
+          title: "update",
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(401);
+  })
 });
 
 /************************************** DELETE /jobs/:id */
@@ -87,6 +171,13 @@ describe("DELETE /jobs/:id", () => {
         deleted: testJobIds[0] 
       }
     );
-  })
+  });
+
+  test("not work for non-admin", async function() {
+    const resp = await request(app)
+        .delete(`/jobs/${testJobIds[0]}`)
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
 });
 
